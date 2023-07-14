7c13fb19b3bf184aece72619eb74b9d6b2782793
007 <dr007cc@gmail.com>
2023-07-14 22:51:44 +0330

    fix: add shader proc in apollo header

--- src/include/apollo.h
+++ src/include/apollo.h
@@ -3,5 +3,9 @@
 #define __APOLLO_APOLLO_H__
 
 #include "common.h"
+#include "logger.h"
+
+void shader_load(uint32_t program, const char *path, GLenum type);
+void shader_link(uint32_t program);
 
 #endif //__APOLLO_APOLLO_H__
