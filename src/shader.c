b37ec6fb9d6995fd0658004240efd0c0430caa16
007 <dr007cc@gmail.com>
2023-08-16 21:02:55 +0330

    fix: update shader includes

--- src/shader.c
+++ src/shader.c
@@ -1,37 +1,15 @@
 
 
-#include <stdbool.h>
-
 #ifndef _GNU_SOURCE
 #define _GNU_SOURCE
 #endif
 
-#include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
-#include <stdarg.h>
-#include <stdbool.h>
-
 #include <assert.h>
-#include <math.h>
-#include <time.h>
-#include <inttypes.h>
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <signal.h>
-#include <endian.h>
-
-#include <sys/random.h>
-#include <sys/stat.h>
-#include <sys/time.h>
-#include <sys/socket.h>
-#include <sys/un.h>
-#include <sys/types.h>
-
-
-
 
 #include "gl.h"
 #include "apollo.h"
