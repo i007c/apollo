f831b728f41a868347b5186e29224aaaeaf78002
007 <dr007cc@gmail.com>
2024-06-14 19:56:51 +0330

    feat: log

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use vulkano::{swapchain::Surface, VulkanLibrary};
+use winit::event_loop::EventLoop;
+
 fn main() {
-    println!("Hello, world!");
+    dotenvy::from_path(".env").expect("could not load .env");
+    pretty_env_logger::init();
+
+    let event_loop = EventLoop::new().expect("event loop");
+    let library = VulkanLibrary::new().expect("vulkan library");
+
+    let required_extensions = Surface::required_extensions(&event_loop);
+    log::debug!("required_extensions: {required_extensions:#?}");
 }
